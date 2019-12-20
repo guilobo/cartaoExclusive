4b040bf248f44062aae83c8c614b0fbb55ae8797
Guilherme Lôbo <gui.wolflobo@gmail.com>
2019-12-20 18:24:00 -0300

    ordem alfabetica e outras coisas

--- apiADM.php
+++ apiADM.php
@@ -7,7 +7,7 @@ if (isset($_POST["acao"])) {
 
   switch ($acao){
     case "clientes":
-      $cliente_db = $wpdb->get_results("SELECT a.id,a.user_login,a.user_email,REPLACE(REPLACE(b.meta_value , '-' , '' ),'.','') as cpf,c.meta_value as validade,d.meta_value as whatsapp FROM `wpnn_users` as a JOIN `wpnn_usermeta` AS b ON a.id = b.user_id JOIN `wpnn_usermeta` AS c ON a.id = c.user_id JOIN `wpnn_usermeta` AS d ON a.id = c.user_id where c.meta_key = 'validade' and b.meta_key = 'cpf' and d.meta_key = 'whatsapp' GROUP by a.id");
+      $cliente_db = $wpdb->get_results("SELECT a.id,a.user_login,a.user_email,REPLACE(REPLACE(b.meta_value , '-' , '' ),'.','') as cpf,c.meta_value as validade,d.meta_value as whatsapp FROM `wpnn_users` as a JOIN `wpnn_usermeta` AS b ON a.id = b.user_id JOIN `wpnn_usermeta` AS c ON a.id = c.user_id JOIN `wpnn_usermeta` AS d ON a.id = c.user_id where c.meta_key = 'validade' and b.meta_key = 'cpf' and d.meta_key = 'whatsapp' GROUP by a.id ORDER by a.user_login ASC");
       echo json_encode($cliente_db);
       break;
 
