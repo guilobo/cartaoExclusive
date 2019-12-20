c9f70e1fe468e2a5d7223995eae1159b74402497
Guilherme Lôbo <gui.wolflobo@gmail.com>
2019-12-20 18:10:00 -0300

    aparcendo parceiros do banco

--- js/controleADM.js
+++ js/controleADM.js
@@ -54,6 +54,111 @@ $(document).ready(function(){
         $("#listaClientes").append(itemCliente);
       })
 
+  });
+
+  $.post("https://draisistoledo.com/cartao/apiADM.php", {acao:"parceiros"})
+  .done(function(data){
+    var parceiros = JSON.parse(data);
+
+      $.each(parceiros, function(index, parceiro){
+        var itemParceiro = "<li>"+
+          "<div class='collapsible-header'>"+
+            "<i class='material-icons'>store_mall_directory</i>"+
+            parceiro.nome;
+            if(parceiro.aprovado == null){
+              itemParceiro +="<span class='badge red'><a class='white-text'>Pendente</a></span></div>";
+            }else{
+              itemParceiro +="<span class='badge green'><a class='white-text'>Ativo</a></span></div>";
+            }
+            itemParceiro +="<div class='collapsible-body'>"+
+            "<div class='row'>"+
+              "<form class='col s12'>"+
+                "<div class='row'>"+
+                  "<div class='input-field col s6'>"+
+                    "<img class='materialboxed imgLogoAdmParceiro' src='uploads/"+parceiro.logo+"'>"+
+                  "</div>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='email' type='email' class='validate' value='pedro@bol.com.br'>"+
+                    "<label for='email'>Nome da Empresa</label>"+
+                  "</div>"+
+                "</div>"+
+                "<div class='row'>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='nome' type='text' class='validate' value='Pedro de Lara'>"+
+                    "<label for='nome'>Email</label>"+
+                  "</div>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='email' type='email' class='validate' value='pedro@bol.com.br'>"+
+                    "<label for='email'>CNPJ</label>"+
+                  "</div>"+
+                "</div>"+
+                "<div class='row'>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='nome' type='text' class='validate' value='Pedro de Lara'>"+
+                    "<label for='nome'>Endereço</label>"+
+                  "</div>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='email' type='email' class='validate' value='pedro@bol.com.br'>"+
+                    "<label for='email'>Cidade</label>"+
+                  "</div>"+
+                "</div>"+
+                "<div class='row'>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='nome' type='text' class='validate' value='Pedro de Lara'>"+
+                    "<label for='nome'>Estado</label>"+
+                  "</div>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='email' type='email' class='validate' value='pedro@bol.com.br'>"+
+                    "<label for='email'>Telefone</label>"+
+                  "</div>"+
+                "</div>"+
+                "<div class='row'>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='nome' type='text' class='validate' value='Pedro de Lara'>"+
+                    "<label for='nome'>Ramo de Atuação</label>"+
+                  "</div>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='email' type='email' class='validate' value='pedro@bol.com.br'>"+
+                    "<label for='email'>Horário de Funcionamento</label>"+
+                  "</div>"+
+                "</div>"+
+                "<div class='row'>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='nome' type='text' class='validate' value='Pedro de Lara'>"+
+                    "<label for='nome'>Desconto</label>"+
+                  "</div>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='email' type='email' class='validate' value='pedro@bol.com.br'>"+
+                    "<label for='email'>Tipo de Desconto</label>"+
+                  "</div>"+
+                "</div>"+
+                "<div class='row'>"+
+                  "<div class='input-field col s6'>"+
+                    "<input id='email' type='email' class='validate' value='pedro@bol.com.br'>"+
+                    "<label for='email'>Observação do Desconto</label>"+
+                  "</div>"+
+                "</div>"+
+                "<div class='row'>"+
+                  "<div class='col s6'>"+
+                    "<a class='btn-floating btn-small waves-effect waves-light red'><i class='material-icons'>delete</i></a> Excluir"+
+                  "</div>"+
+                  "<div class='col s6'>"+
+                      "<a class='waves-effect waves-light btn right'><i class='material-icons left'>check</i>APROVAR</a>"+
+                  "</div>"+
+                "</div>"+
+              "</form>"+
+            "</div>"+
+          "</div>"+
+        "</li>";
+
+        if(parceiro.aprovado == null){
+        $("#divAgardandoParceiro").removeClass("some");
+        $("#listaParceirosAguardando").append(itemParceiro);
+      }else {
+        $("#listaParceiros").append(itemParceiro);
+      }
+      })
+
   })
 
 })
