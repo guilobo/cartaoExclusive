150065b048990e4cd472b90a30d600b8c2363646
maiconfr <38378643+maiconfr@users.noreply.github.com>
2019-12-20 16:31:17 -0300

    Correção da api Clientes

--- apiADM.php
+++ apiADM.php
@@ -2,12 +2,12 @@
 header("Access-Control-Allow-Origin: *");
 require_once($_SERVER['DOCUMENT_ROOT']."/wp-load.php");
 
-if (isset($_POST("acao"))) {
-  $acao = $_POST("acao");
+if (isset($_POST["acao"])) {
+  $acao = $_POST["acao"];
 
   switch ($acao){
     case "clientes":
-      $cliente_db = $wpdb->get_results("SELECT a.id,a.user_login,a.user_email,b.meta_value as cpf,c.meta_value as validade,d.meta_value as whatsapp FROM `wpnn_users` as a JOIN `wpnn_usermeta` AS b ON a.id = b.user_id JOIN `wpnn_usermeta` AS c ON a.id = c.user_id JOIN `wpnn_usermeta` AS d ON a.id = c.user_id where b.meta_key = 'validade' and c.meta_key = 'cpf' and d.meta_key = 'whatsapp'");
+      $cliente_db = $wpdb->get_results("SELECT a.id,a.user_login,a.user_email,b.meta_value as cpf,c.meta_value as validade,d.meta_value as whatsapp FROM `wpnn_users` as a JOIN `wpnn_usermeta` AS b ON a.id = b.user_id JOIN `wpnn_usermeta` AS c ON a.id = c.user_id JOIN `wpnn_usermeta` AS d ON a.id = c.user_id where b.meta_key = 'validade' and c.meta_key = 'cpf' and d.meta_key = 'whatsapp' GROUP by a.id");
       echo json_encode($cliente_db);
       break;
 
